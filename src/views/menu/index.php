f60fd758338289c5f170baf7f9b31ccf8e54808f
zacksleo <zacksleo@gmail.com>
2017-08-05 12:48:09 +0800

    fix menu bug

--- src/views/menu/index.php
+++ src/views/menu/index.php
@@ -3,7 +3,7 @@
 use zacksleo\yii2\wechat\models\WechatMenu;
 
 Yii::$app->getModule('treemanager')->treeViewSettings = [
-    'nodeView' => '@vendor/zacksleo/yii2-wechat/views/menu/_form'
+    'nodeView' => '@vendor/zacksleo/yii2-wechat/src/views/menu/_form'
 ];
 Yii::$app->getModule('treemanager')->init();
 
