506701631047b60c9dc9060ebb2bbc017086defe
zacksleo <zacksleo@gmail.com>
2017-09-13 22:19:07 +0800

    update tests

--- tests/models/WechatBasicConfigTest.php
+++ tests/models/WechatBasicConfigTest.php
@@ -5,6 +5,7 @@ namespace tests\models;
 
 
 use tests\TestCase;
+use zacksleo\yii2\wechat\models\WechatBasicConfig;
 
 /**
  * Class WechatBasicConfigTest
@@ -13,5 +14,33 @@ use tests\TestCase;
  */
 class WechatBasicConfigTest extends TestCase
 {
+    public function testRules()
+    {
+        $model = new WechatBasicConfig();
+        $model->appId = 'appId';
+        $model->appSecret = 'appSecret';
+        $model->token = 'token';
+        $model->encodingAESKey = 'encodingAESKey';
+        $this->assertTrue($model->validate());
+    }
 
+    public function testFields()
+    {
+        $model = new WechatBasicConfig();
+        $fields = $model->fields();
+        $this->assertArrayHasKey('appId', $fields);
+        $this->assertArrayHasKey('appSecret', $fields);
+        $this->assertArrayHasKey('token', $fields);
+        $this->assertArrayHasKey('encodingAESKey', $fields);
+    }
+
+    public function testAttributes()
+    {
+        $model = new WechatBasicConfig();
+        $attributes = $model->attributes();
+        $this->assertArrayHasKey('appId', $attributes);
+        $this->assertArrayHasKey('appSecret', $attributes);
+        $this->assertArrayHasKey('token', $attributes);
+        $this->assertArrayHasKey('encodingAESKey', $attributes);
+    }
 }
--- tests/models/WechatPayConfigTest.php
+++ tests/models/WechatPayConfigTest.php
@@ -3,6 +3,7 @@
 namespace tests\models;
 
 use tests\TestCase;
+use zacksleo\yii2\wechat\models\WechatPayConfig;
 
 /**
  * Class WechatPayConfigTest
@@ -11,5 +12,30 @@ use tests\TestCase;
  */
 class WechatPayConfigTest extends TestCase
 {
+    public function testRules()
+    {
+        $model = new WechatPayConfig();
+        $model->merchantId = 'merchantId';
+        $model->merchantSecret = 'merchantSecret';
+        $model->active = 1;
+        $this->assertTrue($model->validate());
+    }
 
+    public function testFields()
+    {
+        $model = new WechatPayConfig();
+        $fields = $model->fields();
+        $this->assertArrayHasKey('merchantId', $fields);
+        $this->assertArrayHasKey('merchantSecret', $fields);
+        $this->assertArrayHasKey('active', $fields);
+    }
+
+    public function testAttributes()
+    {
+        $model = new WechatPayConfig();
+        $attributes = $model->attributes();
+        $this->assertArrayHasKey('merchantId', $attributes);
+        $this->assertArrayHasKey('merchantSecret', $attributes);
+        $this->assertArrayHasKey('active', $attributes);
+    }
 }
